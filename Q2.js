0ffdaecf7817178459e15e6e93d81f84de2b9973
dhrutawalwatkar <41356820+dhrutawalwatkar@users.noreply.github.com>
2020-07-25 19:21:59 +0530

    Add files via upload

---
+++ Q2.js
@@ -0,0 +1,16 @@
+var c=prompt("enter a no");
+document.write('<h1> Table of </h1>',c),'<br>';
+for( i=1;i<=10;i++){
+    document.write(c,'X',i ,'=',c*i,'<br>');
+}
+
+/*document.write(c,'X 2 =',c*2,'<br>');
+document.write(c,'X 2 =',c*3,'<br>');
+document.write(c,'X 2 =',c*4,'<br>');
+document.write(c,'X 2 =',c*5,'<br>');
+document.write(c,'X 2 =',c*6,'<br>');
+document.write(c,'X 2 =',c*7,'<br>');
+document.write(c,'X 2 =',c*8,'<br>');
+document.write(c,'X 2 =',c*9,'<br>');
+document.write(c,'X 2 =',c*10,'<br>');
+*/
